d3593e4719e0da4852d45e138c1f8e416a4f6f5b
dong881 <joe888joejoe@gmail.com>
2024-02-21 15:44:06 +0800

    change nfap test log path

--- nfapi-fixes/simpleSample.sh
+++ nfapi-fixes/simpleSample.sh
@@ -2,7 +2,7 @@ sudo ifconfig lo: 127.0.0.2 netmask 255.0.0.0 up
 
 # Common path
 Target_PATH="/home/yen0224/NTUST/nfapi-fixes"
-LOG_PATH="$Target_PATH/LOG"
+LOG_PATH="$Target_PATH/LTE-config/LOG"
 OAI_PATH="$Target_PATH/openairinterface5g/cmake_targets"
 
 # 進入目錄
@@ -26,14 +26,14 @@ sudo stdbuf -oL $pnf_cmd 2>&1 | ts &> "$LOG_PATH/PNF-nfapi-fixes-simple.log" &
 
 sudo echo "Start VNF..."
 #  --nokrnmode 1
-vnf_cmd="./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-LTE-EPC/CONF/rcc.band78.tm1.106PRB.nfapi.conf --nfapi VNF --noS1 --phy-test"
+vnf_cmd="./nr-softmodem -O ../../../targets/PROJECTS/GENERIC-LTE-EPC/CONF/rcc.band78.tm1.106PRB.nfapi.conf --nfapi VNF --noS1 --nokrnmode 1 --phy-test"
 sudo stdbuf -oL $vnf_cmd 2>&1 | ts &> "$LOG_PATH/VNF-nfapi-fixes-simple.log" &
 
 # sudo echo "Start UE..."
 # ue_cmd="./nr-uesoftmodem --rfsim --phy-test -d --rfsimulator.serveraddr 127.0.0.1"
 # sudo stdbuf -oL $ue_cmd 2>&1 | ts &> "$LOG_PATH/UE-nfapi-fixes-simple.log" &
 
-wait_sec=20
+wait_sec=25
 echo "The script automatically closes after $wait_sec seconds..."
 # Wait for the specified number of seconds
 sleep $wait_sec
