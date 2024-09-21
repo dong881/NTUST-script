da783b9720183b0813b00033120eea22697c7d95
dong881 <joe888joejoe@gmail.com>
2024-09-21 17:06:53 +0800

    Refactor First-time-execute-me.sh to clone repositories if they don't exist

--- First-time-execute-me.sh
+++ First-time-execute-me.sh
@@ -2,9 +2,17 @@
 sudo ifconfig lo:ODU  "192.168.130.81"
 sudo ifconfig lo:OAI_CU "192.168.130.83"
 
-git clone -b OAI-CU-for-OSC https://github.com/dong881/openairinterface5g-NTUST.git ../O-CU
-git clone -b OSC-use-nfapi https://github.com/dong881/openairinterface5g-NTUST.git ../OAI_L1_UE
-git clone -b FixRAR https://github.com/dong881/NTUST-OSC-DU-nFAPI.git ../O-DU
+if [ ! -d "../O-CU" ]; then
+    git clone -b OAI-CU-for-OSC https://github.com/dong881/openairinterface5g-NTUST.git ../O-CU
+fi
+
+if [ ! -d "../OAI_L1_UE" ]; then
+    git clone -b OSC-use-nfapi https://github.com/dong881/openairinterface5g-NTUST.git ../OAI_L1_UE
+fi
+
+if [ ! -d "../O-DU" ]; then
+    git clone -b FixRAR https://github.com/dong881/NTUST-OSC-DU-nFAPI.git ../O-DU
+fi
 
 sudo apt-get install moreutils -y
 cd ./Tools
