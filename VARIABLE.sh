2d3c9fc5299e27067f99cc0d9fcf0855d495efaa
dong881 <joe888joejoe@gmail.com>
2024-09-21 17:11:04 +0800

    Refactor VARIABLE.sh to update file paths for nfapi-fixes logs

--- VARIABLE.sh
+++ VARIABLE.sh
@@ -1,11 +1,8 @@
-ROOT_PATH=$(pwd | awk -F'/' '{print $3}')
 HOME_DIR=$(eval echo ~$USER)
-export PATH_TO_SCRIPT="$HOME_DIR/$ROOT_PATH/NTUST-script"
-export PATH_TO_CU="$HOME_DIR/$ROOT_PATH/O-CU"
-export PATH_TO_O_DU_L2="$HOME_DIR/$ROOT_PATH/O-DU/l2"
-export PATH_TO_L1_UE="$HOME_DIR/$ROOT_PATH/OAI_L1_UE"
-echo "ROOT_PATH: $ROOT_PATH"
-echo "HOME_DIR: $HOME_DIR"
+export PATH_TO_SCRIPT="$HOME_DIR/NTUST-script"
+export PATH_TO_CU="$HOME_DIR/O-CU"
+export PATH_TO_O_DU_L2="$HOME_DIR/O-DU/l2"
+export PATH_TO_L1_UE="$HOME_DIR/OAI_L1_UE"
 # export PATH_TO_CU="/home/$ROOT_PATH/bmwlab_tony_cu_du"
 # export PATH_TO_O_DU_L2="/home/$ROOT_PATH/mwnl-odu-at-oai-based-on-scf/l2"
 # export PATH_TO_L1_UE="/home/$ROOT_PATH/openairinterface5g"
