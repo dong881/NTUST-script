334ea9f968650ec2eb4aa5c8d19648b3d38a0761
dong881 <joe888joejoe@gmail.com>
2025-03-07 16:52:17 +0800

    add configuration files and scripts for network setup and build process

--- VARIABLE.sh
+++ VARIABLE.sh
@@ -2,12 +2,12 @@ HOME_DIR=$(eval echo ~$USER)
 export PATH_TO_SCRIPT="$HOME_DIR/NTUST-script"
 export PATH_TO_CU="$HOME_DIR/O-CU"
 export PATH_TO_O_DU_L2="$HOME_DIR/O-DU/l2"
-export PATH_TO_L1_UE="$HOME_DIR/OAI_L1_UE"
-# export PATH_TO_CU="$HOME_DIR/bmwlab_tony_cu_du"
-export PATH_TO_CU="$HOME_DIR/oaicu_develope/openairinterface5g"
+export PATH_TO_L1="$HOME_DIR/OAI_L1_UE"
+export PATH_TO_UE="$HOME_DIR/OAI_L1_UE"
 # export PATH_TO_O_DU_L2="$HOME_DIR/mwnl-odu-at-oai-based-on-scf/l2"
 export PATH_TO_L1_UE="$HOME_DIR/OnlyOAI/openairinterface5g"
+# =================================================================== oaicu_develope
+export PATH_TO_CU="$HOME_DIR/oaicu_develope/openairinterface5g"
+export PATH_TO_O_DU_L2="$HOME_DIR/o-du-l2"
 export PATH_TO_L1="$HOME_DIR/openairinterface5g"
 export PATH_TO_UE="$HOME_DIR/OnlyOAI/openairinterface5g"
-
-export PATH_TO_O_DU_L2="$HOME_DIR/o-du-l2"
