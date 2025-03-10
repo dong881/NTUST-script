85057804bcde2524f86dce991eeb25e6a6fc6401
dong881 <joe888joejoe@gmail.com>
2025-03-10 20:33:46 +0800

    Add robotAutoCheck script to monitor log for error conditions and manage execution flow

---
+++ robotAutoCheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+l="/home/hpe/NTUST-script/LOG/5-OAI_UE.log"
+ec=("synch Failed" "RAR reception failed" "Lost socket")
+bash ./exit
+while :; do
+    bash ./all-in-one
+    while :; do
+        flag=0
+        for e in "${ec[@]}"; do
+            if grep -q "$e" "$l"; then
+                echo -e "Error condition '$e' detected.\r"
+                bash ./exit
+                flag=1
+                break
+            fi
+        done
+        [ $flag -eq 1 ] && break
+        grep -q "cumulated bad DCI" "$l" && { echo -e "cumulated bad DCI detected. Exiting loop.\r"; exit 0; }
+        sleep 1
+    done
+done
